42ff4de3ffd07f58c7c223284224f3f62f6630bb
Vladyslav Vatslavyi <vatsikvlad@gmail.com>
2026-08-28 12:30:50 +0200

    Deleted age from my bio

--- Main.py
+++ Main.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request
-import MyBio
 
 app: Flask = Flask(__name__)
-myBio: MyBio.MyBio = MyBio.MyBio()
 
 @app.route("/", methods=['GET'])
 def Index() -> str:
-    return render_template('home.html', myBio = myBio)
+    return render_template('home.html')
 
 @app.errorhandler(404)
-def NotFound(e: Exception) -> str:
+def NotFound(e: Exception) -> tuple[str, int]:
     return render_template("notFound.html"), 404
+
+@app.route("/404.html")
+def NotFoundPage() -> str:
+    return render_template("notFound.html")
 
 if __name__ == "__main__":
     app.run()
